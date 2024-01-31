8754bfea0b5ca4b5564345e059082c8899b8e28c
Tanvi Gavali <tanviprakash24@gmail.com>
2024-01-31 03:24:14 -0500

    Added script, style and home

---
+++ Assignment1/script.js
@@ -0,0 +1,7 @@
+
+function openNewWebsite() {
+    var bleep = new Audio();
+    bleep.src = "assets/arcade-bleep.mp3";
+    bleep.play();
+    window.open('https://aaiko.itch.io/bouncebuds', '_blank');
+}

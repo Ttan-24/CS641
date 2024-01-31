a05395acb3badc0084c3a61f11c70cb09073e03e
Tanvi Gavali <tanviprakash24@gmail.com>
2024-01-31 16:51:46 -0500

    To-do worked on:
    - Implemented samp and kbd tag
    - added content in the web
    - reformatted the code
    - added css-animation for the cursor

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,7 +1,17 @@
 
-function openNewWebsite() {
-    var bleep = new Audio();
-    bleep.src = "assets/arcade-bleep.mp3";
-    bleep.play();
+function openItchWebsite() {
+    var audioClick = new Audio();
+    audioClick.src = "assets/arcade-bleep.mp3";
+    audioClick.play();
     window.open('https://aaiko.itch.io/bouncebuds', '_blank');
+}
+
+function onNextImage() {
+    var imageTag = document.getElementById("gameImage2");
+    imageTag.style.display = "block";
+}
+
+function onPreviousImage() {
+    var imageTag = document.getElementById("gameImage");
+    imageTag.style.display = "block";
 }
